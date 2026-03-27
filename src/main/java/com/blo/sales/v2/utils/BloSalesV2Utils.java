244c1daa4c93abeb07477990925e4c4ac23d9249
orlandoag89 <orlandoag89@gmail.com>
2026-03-26 20:52:44 -0600

    se hace migracion a maven

--- src/main/java/com/blo/sales/v2/utils/BloSalesV2Utils.java
+++ src/main/java/com/blo/sales/v2/utils/BloSalesV2Utils.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
  */
 public final class BloSalesV2Utils {
     
-    public static final String VERSION = "v2.7.1-RELEASE";
+    public static final String VERSION = "v2.8.0-SNAPSHOT";
     
     private static final String RELEASE = "RELEASE";
     
