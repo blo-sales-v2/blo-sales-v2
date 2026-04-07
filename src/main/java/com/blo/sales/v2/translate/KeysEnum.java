2959acab11d4dc34b228361300d2440ba222dc54
orlandoag89 <orlandoag89@gmail.com>
2026-04-07 14:23:05 -0600

    se agrega alert de advertencia que no se podra ver las cajas como invitado

--- src/main/java/com/blo/sales/v2/translate/KeysEnum.java
+++ src/main/java/com/blo/sales/v2/translate/KeysEnum.java
@@ -19,7 +19,7 @@ public enum KeysEnum {
     DLG_SELECTOR("dlg-filter.lbl.filter"), DLG_CASHBOX_TRG_SALES_TODAY("dlg-cashbox.trg.sales-today"),
     // cashbox
     CASHBOX_BTN_CLOSE_NOW("cashbox.btn.close-now"), CASHBOX_DLG_IMPORT_FROM_NOTES("cashbox.dlg.import-from-notes"), CASHBOX_DLG_CLOSING_CASHBOX("cashbox.dlg.closing-cashbox"), CASHBOXES_BTN_VIEW_DETAILS("cashboxes.btn.view-details"), CASHBOXES_BTN_GRAPHICS("cashboxes.btn.grapics"),
-    CASHBOXES_LBL_ACTIVES("cashboxes.lbl.actives"), CASHBOXES_LBL_COSTS("cashboxes.lbl.costs"), CASHBOXES_LBL_CASHBOX_OF("cashboxes.lbl.cashbox-of"),
+    CASHBOXES_LBL_ACTIVES("cashboxes.lbl.actives"), CASHBOXES_LBL_COSTS("cashboxes.lbl.costs"), CASHBOXES_LBL_CASHBOX_OF("cashboxes.lbl.cashbox-of"), CASHBOX_DLG_CONTINUE("cashbox.dlg.continue"),
     // notes
     NOTES_LBL_INSTRUCTIONS("notes.lbl.instructions"), NOTES_BTN_SAVE_NOTE("notes.btn.save-note"), DLG_NOTE_BTN_DELETE("dlg-note.btn.delete"), NOTES_DLG_EDITING_NOTE("notes.dlg.editing-note"),
     // debtors
